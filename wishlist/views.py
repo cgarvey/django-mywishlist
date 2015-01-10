65a88d7aeb68d55d902aa48691d1309b242ea9f4
Cathal Garvey <github.com-9102@cgarvey.com>
2015-01-10 08:02:13 +0000

    Add default index view (the wishlist public listing)

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+def index( request ):
+	''' Default view (for visitors to the root URL) which presents login form, processes login form, or redirects
+	to dashboard if already logged in'''
+
+	from wishlist.models import Item
+
+	data = []
+	ls_items = Item.objects.filter( is_active=True ).order_by( "category__sort_order", "category__id", "sort_order" ).prefetch_related( "category" )
+
+	last_category_id = 0
+	if( ls_items ):
+		for item in ls_items:
+			if( last_category_id != item.category.id ):
+				data.append( [] )
+				last_category_id = item.category.id
+			arr = data[-1]
+			arr.append( item )
+
+	return render_to_response( "wishlist/index.html", { "data":data } )
