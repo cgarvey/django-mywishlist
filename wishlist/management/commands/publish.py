a221bf672e170f4d2a0d4b546a8b2e0883b52995
Cathal Garvey <github.com-9102@cgarvey.com>
2015-01-10 07:59:09 +0000

    Add custom manage.py command to publish the HTML to the publish/ folder (for onward FTP/SCP to external static website)

---
+++ wishlist/management/commands/publish.py
@@ -0,0 +1,37 @@
+from django.core.management.base import NoArgsCommand
+
+#pylint: disable-msg=W0232
+#pylint: disable-msg=W0613
+class Command(NoArgsCommand):
+	def handle_noargs( self, **options ):
+		from django.template.loader import render_to_string
+		from wishlist.models import Item
+		# import apperrific.util as U
+
+		filename = "publish/index.html"
+
+		print "Retrieving Items"
+		ls_itm = Item.objects.filter( is_active=True ).order_by( "category__sort_order", "category__id", "sort_order" ).prefetch_related( "category" )
+		if( ls_itm ):
+			print "OK, have %d" % len( ls_itm )
+
+			print "Sorting items"
+			data = []
+			last_category_id = 0
+			if( ls_itm ):
+				for itm in ls_itm:
+					if( last_category_id != itm.category.id ):
+						data.append( [] )
+						last_category_id = itm.category.id
+					arr = data[-1]
+					arr.append( itm )
+
+			print "Rendering template"
+			html = render_to_string( "wishlist/index.html", { "data":data } )
+
+			print "Writing to file (%s)" % filename
+			f = open( filename, "w" )
+			f.write( html )
+			print "Done."
+
+		else: print "OK (none)."
